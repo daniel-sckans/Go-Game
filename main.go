94457cb56f035fb67f723e6447f7ae46c84b9ee6
SeJoMaCode <sethmanche02@gmail.com>
2021-02-25 01:04:14 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	currentBoxes := NewBoxes{
+		X1: fmt.Sprintf("%f", r1.Float64()*.3),
+		Y1: fmt.Sprintf("%f", r1.Float64()*.8),
+		W1: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+		H1: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+		C:  fmt.Sprintf("%f", r1.Float64()),
+		X2: fmt.Sprintf("%f", r1.Float64()*(.8-.5)+.5),
+		Y2: fmt.Sprintf("%f", r1.Float64()*.8),
+		W2: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+		H2: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./site")))
+
+	fmt.Println("Running")
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			for {
+				_, msg, err := conn.ReadMessage()
+
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				if string(msg) == "loaded" {
+					fmt.Println("User Connected")
+					conn.WriteJSON(currentBoxes)
+				}
+				if string(msg) == "green" {
+					println("New Boxes")
+					conn.WriteJSON(SingleMessage{
+						Message: "+1",
+					})
+					currentBoxes = NewBoxes{
+						X1: fmt.Sprintf("%f", r1.Float64()*.3),
+						Y1: fmt.Sprintf("%f", r1.Float64()*.8),
+						W1: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+						H1: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+						C:  fmt.Sprintf("%f", r1.Float64()),
+						X2: fmt.Sprintf("%f", r1.Float64()*(.8-.5)+.5),
+						Y2: fmt.Sprintf("%f", r1.Float64()*.8),
+						W2: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+						H2: fmt.Sprintf("%f", r1.Float64()*(.2-.1)+.1),
+					}
+				} else if string(msg) == "red" {
+					conn.WriteJSON(SingleMessage{
+						Message: "-5",
+					})
+				}
+			}
+		}(conn)
+		go func(conn *websocket.Conn) {
+			ch := time.Tick(time.Millisecond)
+
+			for range ch {
+				conn.WriteJSON(currentBoxes)
+			}
+		}(conn)
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
+
+type NewBoxes struct {
+	X1 string `json:"x1"`
+	Y1 string `json:"y1"`
+	W1 string `json:"w1"`
+	H1 string `json:"h1"`
+	C  string `json:"c"`
+	X2 string `json:"x2"`
+	Y2 string `json:"y2"`
+	W2 string `json:"w2"`
+	H2 string `json:"h2"`
+}
+
+type SingleMessage struct {
+	Message string `json:"message"`
+}
---
+++ site/main.js
@@ -0,0 +1,134 @@
+let c;
+let ctx;
+
+const ws = new WebSocket("ws://localhost:3000/ws")
+let version = '0.1.0'
+
+class Box {
+    constructor(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.width = w
+        this.height = h
+    }
+
+    update() {
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    checkClick(x, y){
+        if(x <= this.x+this.width && x >= this.x){
+            if(y <= this.y+this.height && y >= this.y){
+                return true
+            }
+        }
+    }
+}
+
+window.onload = init
+
+ws.onopen = () => {console.log("Websocket Opened")}
+ws.onclose = () => {console.log("Websocket Closed")}
+ws.onerror = (error) => {console.log("Websocket Error: ", error)}
+
+let box1 = new Box(0,0,0,0)
+let box2 = new Box(0,0,0,0)
+let color
+
+let started
+document.onclick = (e) => {
+    if(!started){
+        started = true
+    } else {
+        const data = {}
+        if(box1.checkClick(e.clientX, e.clientY)){
+            if(color>=.5){
+                ws.send("green")
+            } else {
+                ws.send("red")
+            }
+        }
+        if(box2.checkClick(e.clientX, e.clientY)){
+            if(color<.5){
+                ws.send("green")
+            } else {
+                ws.send("red")
+            }
+        }
+    }
+}
+
+function resize(){
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+
+let score = 0
+
+function init(){
+    c = document.querySelector('canvas')
+    ctx = c.getContext('2d')
+    resize()
+
+    ws.send('loaded')
+    ws.onmessage = (msg) => {
+        message_data = JSON.parse(msg.data)
+        box1.x = message_data.x1 * c.width
+        box1.y = message_data.y1 * c.height
+        box1.width = message_data.w1 * c.width
+        box1.height = message_data.h1 * c.height
+        box2.x = message_data.x2 * c.width
+        box2.y = message_data.y2 * c.height
+        box2.width = message_data.w2 * c.width
+        box2.height = message_data.h2 * c.height
+        color = message_data.c
+        if(message_data.message){
+            score = eval(score+message_data.message)
+        }
+    }
+
+    window.requestAnimationFrame(gameLoop)
+}
+
+function gameLoop(timeStamp){
+    window.onresize = resize()
+    draw()
+    console.log(score)
+    window.requestAnimationFrame(gameLoop)
+}
+
+function draw(){
+    ctx.fillRect(0,0,c.width,c.height)
+
+    if(color>=.5){
+        ctx.fillStyle = 'green'
+        box1.update()
+        ctx.fillStyle = 'red'
+        box2.update()
+    }else{
+        ctx.fillStyle = 'green'
+        box2.update()
+        ctx.fillStyle = 'red'
+        box1.update()
+    }
+
+    ctx.fillStyle = 'white'
+    ctx.font = 'bold 40px Verdana'
+    ctx.textAlign = "left"
+    ctx.fillText(''+score, 5, 45)
+
+    if(!started){
+        ctx.fillStyle = 'black'
+        ctx.font = 'bold 20px Verdana'
+        ctx.fillRect(0,0,c.width,c.height)
+        ctx.textAlign = "center"
+        ctx.translate(c.width/2,c.height/2)
+        ctx.strokeStyle = '#ffffff'
+        ctx.fillStyle = '#ffffff'
+        ctx.lineWidth = 5
+        ctx.strokeRect(0-90, -25, 180, 50)
+        ctx.fillText('CLICK TO BEGIN', 0, 7, 150)
+        ctx.textAlign = "right"
+        ctx.fillText(version,c.width/2-5, c.height/2-10)
+    }
+}
